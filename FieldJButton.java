cf4e1d9b7c608201a24e076f6f8640033ea00a79
lowercasewords <87104601+lowercasewords@users.noreply.github.com>
2022-05-21 19:21:22 -0700

    starte code

---
+++ FieldJButton.java
@@ -0,0 +1,12 @@
+
+import javax.swing.*;
+import java.awt.*;
+
+public class FieldJButton extends JButton{
+    // private Color color = new Color(Color.BLACK);
+
+    public void setColor()
+    {
+        // Color.
+    }
+}
