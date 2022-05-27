253cfa6bbaa1ae5c3e3a7c6d906ce9a5d0aa0cd8
DisplayMeToWorld! <87104601+lowercasewords@users.noreply.github.com>
2022-05-27 21:52:20 +0000

    main doesn't work!

--- StartUp/Main.java
+++ StartUp/Main.java
@@ -1,13 +1,12 @@
-import javax.swing.*;
+package StartUp;
+
 import org.w3c.dom.Text;
-
+import javax.swing.*;
 import Session.Settings;
-
 import java.util.Random;
-import java.util.concurrent.BlockingQueue;
 import java.awt.*;
 import java.lang.reflect.GenericDeclaration;
-
+import java.lang.*;
 import javax.swing.*;
 
 public class Main {
@@ -17,14 +16,15 @@ public class Main {
 
     private static JFrame _window = new JFrame("Connect - 4");
     
-    public static void main (String[] args) 
+    public static void main (String[] args) throws Exception
     {
       System.out.println("something");
+      throw new Exception();
         // _window.setMinimumSize(new Dimension(MIN_WINDOW_WIDTH, MIN_WINDOW_LENGTH));
         // _window.setPreferredSize(new Dimension(2048, 2048));
         // _window.setVisible(true);
         // _window.setLayout(new BorderLayout());
 
-      Session.Map.MapModel connectFour = new Session.Map.MapModel();
+      // Session.Map.MapModel connectFour = new Session.Map.MapModel();
     }
 }
